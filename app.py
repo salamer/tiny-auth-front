93abba1bc714d72c4179549b7ed8f0b28f813662
salamer <salamer_gaga@163.com>
2023-10-22 12:31:33 +0800

    update: update app

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__, static_folder="fe/dist/assets",
             template_folder="fe/dist", static_url_path="/assets")
 
 
-@app.route("/")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def hello_world():
     return render_template("index.html")
 
